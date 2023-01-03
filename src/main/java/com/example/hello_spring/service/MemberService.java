73854c46b036f830ad76328c211a332f61e1ab7a
723poil <leetkdguq73@naver.com>
2023-01-03 23:02:32 +0900

    Add MemberService

---
+++ src/main/java/com/example/hello_spring/service/MemberService.java
@@ -0,0 +1,49 @@
+package com.example.hello_spring.service;
+
+import com.example.hello_spring.domain.Member;
+import com.example.hello_spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService { // command + shift + T -> test파일 자동 생성
+
+    private final MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+
+        // 같은 이름이 있는 중복 회원 x command + option + v 단축기 -> 반환변수 생성
+//        Optional<Member> result = memberRepository.findByName(member.getName());
+//        result.ifPresent(m -> {
+//            throw new IllegalStateException("이미 존재하는 회원입니다.");
+//        });
+
+        // control + T -> 함수 만들기 단축기
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                        .ifPresent(m -> {
+                            throw new IllegalStateException("이미 존재하는 회원입니다.");
+                        });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}
